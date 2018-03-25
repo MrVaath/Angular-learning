e9b35e8b7d0043d72fd90bf844130ae186c380ff
MrVaath <pat.wylegala@gmail.com>
2018-03-25 23:40:40 +0200

    Section 11 (Changing Pages with Routing)

---
+++ routing-start/src/app/auth-guard.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanActivateChild } from "@angular/router";
+import { Observable } from "rxjs/Observable";
+
+// Services
+import { AuthService } from "./auth.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate, CanActivateChild {
+    constructor(private authService: AuthService, private router: Router) {
+
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+        return this.authService.isAuth().then(
+            (auth: boolean) => {
+                if (auth) {
+                    return true;
+                } else {
+                    this.router.navigate(['/']);
+                }
+            }
+        );
+    }
+
+    canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+        return this.canActivate(route, state);
+    }
+}
